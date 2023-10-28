7b68e3750e1c7214acd4b0f21d082d969a8e237c
Mark View <asir64@163.com>
2023-10-28 16:34:41 +0800

    Updates

---
+++ DailyCatalyst/View/SettingsView.swift
@@ -0,0 +1,29 @@
+//
+//  SettingsView.swift
+//  DailyCatalyst
+//
+//  Created by Asir Bygud on 10/26/23.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        NavigationStack {
+            List {
+                Section {
+                    
+                }
+                
+                Section {
+                    
+                }
+            }
+            .navigationTitle("Settings")
+        }
+    }
+}
+
+#Preview {
+    SettingsView()
+}
