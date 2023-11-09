7b5e98f3eb972f41a778cc8f11b1b18ead137789
Mark View <asir64@163.com>
2023-11-10 04:02:27 +0800

    Create Color+Extensions.swift

---
+++ DailyCatalyst/View/Quoted Views/Color+Extensions.swift
@@ -0,0 +1,44 @@
+//
+//  Color+Extensions.swift
+//  DailyCatalyst
+//
+//  Created by Asir Bygud on 11/10/23.
+//
+
+import Foundation
+import SwiftUI
+import UIKit
+
+extension Color: RawRepresentable {
+
+    public init?(rawValue: String) {
+        
+        guard let data = Data(base64Encoded: rawValue) else{
+            self = .black
+            return
+        }
+        
+        do{
+            let color = try NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(data) as? UIColor ?? .black
+            self = Color(color)
+        }catch{
+            self = .black
+        }
+        
+    }
+
+    public var rawValue: String {
+        
+        do{
+            let data = try NSKeyedArchiver.archivedData(withRootObject: UIColor(self), requiringSecureCoding: false) as Data
+            return data.base64EncodedString()
+            
+        }catch{
+            
+            return ""
+            
+        }
+        
+    }
+
+}
