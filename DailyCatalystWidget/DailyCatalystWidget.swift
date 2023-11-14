fb7bab208ecb2ef4b56896dd922c64cc7365a70c
Mark View <asir64@163.com>
2023-11-14 23:43:44 +0800

    Updates

--- DailyCatalystWidget/DailyCatalystWidget.swift
+++ DailyCatalystWidget/DailyCatalystWidget.swift
@@ -10,85 +10,42 @@ import SwiftUI
 import CoreData
 
 struct Provider: AppIntentTimelineProvider {
-    let dataController = DataController()
-//    let coreDataManager: CoreDataManager
-    
-//    init() {
-//        coreDataManager = CoreDataManager(dataController)
-//    }
-    
     func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date(), configuration: ConfigurationAppIntent(), catalyst: .example)
+        SimpleEntry(date: Date(), catalyst: .example)
     }
     
     func snapshot(for configuration: ConfigurationAppIntent, in context: Context) async -> SimpleEntry {
-        SimpleEntry(date: Date(), configuration: configuration, catalyst: DataController().randomlyPickACatalyst())
+        SimpleEntry(date: Date(), catalyst: DataController().randomlyPickACatalyst())
     }
     
-//    func snapshot(for configuration: ConfigurationAppIntent, in context: Context, completion: @escaping (SimpleEntry) -> Void) { //swiftlint:disable:this line_length
-//        catalystForWidget(for: configuration) { selectedCatalyst in
-//            let entry = SimpleEntry(
-//                date: Date(),
-//                configuration: configuration,
-//                catalyst: selectedCatalyst ?? .example
-//            )
-//            completion(entry)
-//        }
-//        
-////        coreDataManager.fetchCatalysts()
-//        
-////        catalystForWidget(for: configuration) { selectedCatalyst in
-////            let currentDate = Date()
-////            var entries: [SimpleEntry] = []
-////
-////            // Create a date that's 60 minutes in the future.
-////            let nextUpdateDate = Calendar.current.date(byAdding: .minute, value: 60, to: currentDate)!
-////
-////            // Generate an Entry
-////            let entry = SimpleEntry(date: currentDate, configuration: configuration, catalyst: selectedCatalyst ?? .example)
-////            entries.append(entry)
-////
-////            let timeline = Timeline(entries: entries, policy: .after(nextUpdateDate))
-////            completion(timeline)
-////        }
-//    }
-    
     func timeline(for configuration: ConfigurationAppIntent, in context: Context) async -> Timeline<SimpleEntry> {
-        let catalyst = DataController().randomlyPickACatalyst()
-        let entry = SimpleEntry(date: Date(), configuration: configuration, catalyst: catalyst)
-        let entries: [SimpleEntry] = [entry]
-        
-        return Timeline(entries: entries, policy: .atEnd)
-        
-//        coreDataManager.fetchCatalysts()
-        
-//        catalystForWidget(for: configuration) { selectedCatalyst in
-////            let entry = SimpleEntry(
-////                date: Date(),
-////                configuration: configuration,
-////                catalyst: selectedCatalyst ?? .example
-////            )
-////            completion(entry)
-//
-//            let currentDate = Date()
-//            var entries: [SimpleEntry] = []
-//
-//            // Create a date that's 60 minutes in the future.
-//            let nextUpdateDate = Calendar.current.date(byAdding: .minute, value: 60, to: currentDate)!
-//
-//            // Generate an Entry
-//            let entry = SimpleEntry(date: currentDate, configuration: configuration, catalyst: selectedCatalyst ?? .example)
-//            entries.append(entry)
-//
-//            let timeline = Timeline(entries: entries, policy: .after(nextUpdateDate))
-//            completion(timeline)
-//        }
+        // Create a timeline entry for "now."
+        let date = Date()
+        let entry = SimpleEntry(
+            date: date,
+            catalyst: DataController().randomlyPickACatalyst()
+        )
+
+
+        // Create a date that's 5 seconds in the future.
+        let nextUpdateDate = Calendar.current.date(byAdding: .second, value: 5, to: date)!
+
+
+        // Create the timeline with the entry and a reload policy with the date
+        // for the next update.
+        let timeline = Timeline(
+            entries:[entry],
+            policy: .after(nextUpdateDate)
+        )
+
+
+        // Call the completion to pass the timeline to WidgetKit.
+        return timeline
     }
 }
 
 struct SimpleEntry: TimelineEntry {
     let date: Date
-    let configuration: ConfigurationAppIntent
     let catalyst: Catalyst
 }
 
@@ -108,26 +65,13 @@ struct DailyCatalystWidget: Widget {
             CatalystWidgetView(catalyst: entry.catalyst)
                 .containerBackground(.fill.tertiary, for: .widget)
         }
-    }
-}
-
-extension ConfigurationAppIntent {
-    fileprivate static var smiley: ConfigurationAppIntent {
-        let intent = ConfigurationAppIntent()
-        intent.favoriteEmoji = "😀"
-        return intent
-    }
-    
-    fileprivate static var starEyes: ConfigurationAppIntent {
-        let intent = ConfigurationAppIntent()
-        intent.favoriteEmoji = "🤩"
-        return intent
+        .supportedFamilies([.systemSmall, .systemMedium])
     }
 }
 
 #Preview(as: .systemMedium) {
     DailyCatalystWidget()
 } timeline: {
-    SimpleEntry(date: .now, configuration: .smiley, catalyst: .example)
-    SimpleEntry(date: .now, configuration: .starEyes, catalyst: .example)
+    SimpleEntry(date: .now, catalyst: .example)
+    SimpleEntry(date: .now, catalyst: .example)
 }
