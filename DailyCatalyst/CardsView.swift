213fd0146e1ec37223f9ec8ef94da622d40baeca
Mark View <asir64@163.com>
2023-12-31 02:36:54 +0800

    Update CardsView.swift

--- DailyCatalyst/CardsView.swift
+++ DailyCatalyst/CardsView.swift
@@ -8,8 +8,10 @@
 import SwiftUI
 import GoogleGenerativeAI
 import CoreData
+import OSLog
 
 struct CardsView: View {
+    private var logger = Logger(subsystem: Bundle.main.bundleIdentifier!, category: "generative-ai")
     let model = GenerativeModel(name: "gemini-pro", apiKey: "AIzaSyDpW5cnlKt8wLfmOrgevh8L0x_0Q4JFxms")
     @EnvironmentObject var dataController: DataController
     
@@ -19,14 +21,15 @@ struct CardsView: View {
     @State private var summarizePrompt = "这是我最近做的事情, 请你给我一点总结和激励: "
     @State private var summarizeOutput = "Summary"
     
-    @State private var isRotated = false
+    @State private var isLoading = true
+    @State private var errorMessage: String?
     
     var body: some View {
         NavigationStack {
             List {
                 Section {
-                    Button(isRotated ? "Toggle Rotation: On" : "Off") {
-                        isRotated.toggle()
+                    Button(isLoading ? "Off" : "Toggle Rotation: On") {
+                        isLoading.toggle()
                     }
                     
                     HStack {
@@ -35,9 +38,9 @@ struct CardsView: View {
                         ZStack {
                             ForEach(dataController.catalystsForSelectedFilter().indices.prefix(5)) { index in
                                 SummarizePageOneCard(catalyst: dataController.catalystsForSelectedFilter()[index])
-                                    .rotationEffect(Angle(degrees: (isRotated ? -30 : 0) + (isRotated ? 15 : 0) * Double(index)), anchor: .bottom)
+                                    .rotationEffect(Angle(degrees: (isLoading ? 0 : -30) + (isLoading ? 0 : 15) * Double(index)), anchor: .bottom)
                                     .scaleEffect(0.65)
-                                    .animation(.spring(.bouncy(duration: 0.5, extraBounce: 0.15), blendDuration: 3), value: isRotated)
+                                    .animation(.spring(.bouncy(duration: 0.5, extraBounce: 0.15), blendDuration: 3), value: isLoading)
                             }
                         }
                         
@@ -61,32 +64,46 @@ struct CardsView: View {
                 }
                 
                 Section("Summarize") {
-                    Button("Summarize my Catalysts") {
-                        summarizePrompt = "这是我最近做的事情, 请你给我一点总结和激励: "
-                        
-                        for catalyst in dataController.catalystsForSelectedFilter() {
-                            summarizePrompt += catalyst.catalystTitle
-                            summarizePrompt += ", "
-                        }
-                        
-                        Task {
-                            let response = try await model.generateContent(summarizePrompt)
-                            if let text = response.text {
-                                summarizeOutput = text
-                            }
-                        }
-                    }
+                    Button(isLoading ? "Loading…" : "Summarize my Catalysts") {
+                                            }
+                    .disabled(isLoading)
                     
                     Text(.init(summarizeOutput))
                 }
             }
         }
         .onAppear() {
-            withAnimation {
-                isRotated = true
-            }
+            sleep(1)
+            isLoading = false
         }
         .navigationTitle("Summarize by AI")
+    }
+    
+    func summarize() async {
+        defer {
+            isLoading = false
+        }
+        
+        do {
+            errorMessage = nil
+            isLoading = true
+            
+            summarizePrompt = "这是我最近做的事情, 请你给我一点总结和激励: "
+            
+            for catalyst in dataController.catalystsForSelectedFilter() {
+                summarizePrompt += catalyst.catalystTitle
+                summarizePrompt += ", "
+            }
+            
+            let response = try await model.generateContent(summarizePrompt)
+            
+            if let text = response.text {
+                summarizeOutput = text
+            }
+        } catch {
+            logger.error("\(error.localizedDescription)")
+            errorMessage = error.localizedDescription
+        }
     }
 }
 
