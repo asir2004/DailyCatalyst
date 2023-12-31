3b05ea6d4cbefe9cfdae71f0ae9b50e2033743a1
Mark View <asir64@163.com>
2024-01-01 03:10:43 +0800

    Update CardsView.swift

--- DailyCatalyst/CardsView.swift
+++ DailyCatalyst/CardsView.swift
@@ -13,7 +13,15 @@ import OSLog
 struct CardsView: View {
     private var logger = Logger(subsystem: Bundle.main.bundleIdentifier!, category: "generative-ai")
     let model = GenerativeModel(name: "gemini-pro", apiKey: "AIzaSyDpW5cnlKt8wLfmOrgevh8L0x_0Q4JFxms")
+    
+    var isPreview = false
+    
+    init(isPreview: Bool = false) {
+        self.isPreview = isPreview
+    }
+    
     @EnvironmentObject var dataController: DataController
+    @Environment(\.managedObjectContext) private var viewContext
     
     @State private var summarizePrompt = "这是我最近做的事情, 请你给我一点总结和激励: "
     @State private var summarizeOutput = "Summary"
@@ -25,54 +33,53 @@ struct CardsView: View {
     
     var body: some View {
         NavigationStack {
-            List {
-                Section {
-                    HStack {
-                        Spacer()
-                        
-                        ZStack {
-                            ForEach(dataController.catalystsForSelectedFilter().indices.prefix(5)) { index in
-                                SummarizePageOneCard(catalyst: dataController.catalystsForSelectedFilter()[index])
-                                    .rotationEffect(Angle(degrees: (isLoading ? 0 : -30) + (isLoading ? 0 : 15) * Double(index)), anchor: .bottom)
-                                    .scaleEffect(0.65)
-                                    .animation(.spring(.bouncy(duration: 0.5, extraBounce: 0.15), blendDuration: 3), value: isLoading)
-                            }
-                        }
-                        .offset(y: isLoading ? 0 : -10)
-                        
-                        Spacer()
-                    }
-                    .frame(height: 300)
-                }
-                
-                Section("Summarize") {
-                    Button {
-                        Task {
-                            await summarize()
-                        }
-                    } label: {
+            ScrollView(.horizontal) {
+                List {
+                    Section {
                         HStack {
-                            if isLoading {
-                                ProgressView()
-                                    .padding(.trailing, 3)
-                            } else {
-                                Image(systemName: "square.and.pencil")
-                                    .padding(.trailing, 3)
-                            }
+                            Spacer()
                             
-                            Text(isLoading ? "Loading…" : "Summarize my Catalysts")
+                            ZStack {
+                                ForEach(isPreview ? [.example, .example2, .example, .example, .example].indices : dataController.catalystsForSelectedFilter().indices.prefix(5)) { index in
+                                    SummarizePageOneCard(catalyst: dataController.catalystsForSelectedFilter()[index])
+                                        .rotationEffect(Angle(degrees: (isLoading ? 0 : -30) + (isLoading ? 0 : 15) * Double(index)), anchor: .bottom)
+                                        .scaleEffect(0.65)
+                                        .animation(.spring(.bouncy(duration: 0.5, extraBounce: 0.15), blendDuration: 3), value: isLoading)
+                                }
+                            }
+                            .offset(y: isLoading ? 0 : -10)
+                            
+                            Spacer()
                         }
+                        .frame(height: 300)
                     }
-                    .disabled(isLoading)
                     
-                    Text(.init(summarizeOutput))
-                        .foregroundStyle((summarizeOutput == "Summary" || isLoading) ? .secondary : .primary)
+                    Section("Summarize") {
+                        Button {
+                            Task {
+                                await summarize()
+                            }
+                        } label: {
+                            HStack {
+                                if isLoading {
+                                    ProgressView()
+                                        .padding(.trailing, 3)
+                                } else {
+                                    Image(systemName: "square.and.pencil")
+                                        .padding(.trailing, 3)
+                                }
+                                
+                                Text(isLoading ? "Loading…" : "Summarize my Catalysts")
+                            }
+                        }
+                        .disabled(isLoading)
+                        
+                        Text(.init(summarizeOutput))
+                            .foregroundStyle((summarizeOutput == "Summary" || isLoading) ? .secondary : .primary)
+                    }
                 }
             }
             .navigationTitle("Summarize by AI")
-            .onChange(of: dataController.allCatalysts()) {
-                dataController.catalystsForSelectedFilter()
-            }
         }
     }
     
@@ -96,6 +103,12 @@ struct CardsView: View {
             
             if let text = response.text {
                 summarizeOutput = text
+                
+                let newSummary = SummaryOutput(context: viewContext)
+                newSummary.detail = text
+                newSummary.creationDate = .now
+                
+                try? viewContext.save()
             }
         } catch {
             logger.error("\(error.localizedDescription)")
@@ -105,5 +118,5 @@ struct CardsView: View {
 }
 
 #Preview {
-    CardsView()
+    CardsView(isPreview: true)
 }
